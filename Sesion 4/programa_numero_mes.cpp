2717266074d9d4867c92013dce4033569620d498
Ivan-10k <155702468+Ivan-10k@users.noreply.github.com>
2024-03-16 21:00:32 -0500

    Update programa_numero_mes.cpp

--- Sesion 4/programa_numero_mes.cpp
+++ Sesion 4/programa_numero_mes.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() 
 {
     // Leer el número de mes
     int numeroMes;
-    std::cout << "Ingrese el número de mes (1-12): ";
-    std::cin >> numeroMes;
+    cout << "Ingrese el número de mes (1-12): ";
+    cin >> numeroMes;
 
     // Verificar si el número de mes es válido
     if (numeroMes >= 1 && numeroMes <= 12) 
     {
         // Array con los nombres de los meses
-        std::string nombreMeses[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        string nombreMeses[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
         // Mostrar el nombre del mes
-        std::cout << "El nombre del mes es: " << nombreMeses[numeroMes - 1] << std::endl;
-    } else {
-        std::cout << "Número de mes inválido. Debe estar en el rango de 1 a 12." << std::endl;
+        cout << "El nombre del mes es: " << nombreMeses[numeroMes - 1] << endl;
+    } else 
+    {
+        cout << "Número de mes inválido. Debe estar en el rango de 1 a 12." << endl;
     }
 
     return 0;
